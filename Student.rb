29e85c06b53423f9fc6df55506b291554b58e795
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-12-23 16:08:15 +0330

    Create student class

---
+++ Student.rb
@@ -0,0 +1,11 @@
+require './person'
+
+class Student < Person
+  def initialize(*person, classroom)
+    super(*person)
+    @classroom = classroom
+  end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
